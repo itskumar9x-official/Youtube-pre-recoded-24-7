d4cbb096ad8e6d1296ae454cd4367b244dd57cfe
Kumar <its.kumar9x@gmail.com>
2025-09-20 16:41:14 +0530

    Update start_stream_480p.sh

--- start_stream_480p.sh
+++ start_stream_480p.sh
@@ -8,6 +8,7 @@ do
     ffmpeg -re \
       -stream_loop -1 -i mainvideonosound.mp4 \
       -stream_loop -1 -i song1.mp3 \
+      -map 0:v:0 -map 1:a:0 \
       -c:v libx264 -b:v 1200k -maxrate 1200k -bufsize 2400k -s 854x480 -preset veryfast \
       -c:a aac -b:a 128k -pix_fmt yuv420p \
       -g 60 -keyint_min 48 \
